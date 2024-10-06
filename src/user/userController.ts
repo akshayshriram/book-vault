b88ea6ad8d924a3ee583acd9363462703f6b1ae3
Akshay <akshayshriram999@gmail.com>
2024-10-06 16:14:44 +0530

    User model Schema added..

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
+import userModel from './userModel';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -9,6 +10,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     if (!name || !email || !password) {
 
         const error = createHttpError(400, "All fields are required")
+
+        return next(error);
+    }
+    // Database Call
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+        const error = createHttpError(400, "User already Exists with this email...");
 
         return next(error);
     }
