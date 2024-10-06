111ace68ea44cd6d20dcfba867f1ad82f15c78dc
Akshay <akshayshriram999@gmail.com>
2024-10-06 19:01:04 +0530

    Add Login Endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,8 @@ import bcrypt from "bcrypt"
 import { sign } from 'jsonwebtoken';
 import { config } from '../config/config';
 import { User } from './userTypes';
+import { create } from 'domain';
+import mongoose from 'mongoose';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
@@ -29,7 +31,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         }
 
     } catch (err) {
-        return next(createHttpError(500, "Error while fetching user"))
+        return next(createHttpError(500, "Register - Error while fetching user"))
     }
 
     // Create has for the password
@@ -62,7 +64,44 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const loginUser = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "OK" })
+
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+        return next(createHttpError(400, "All Fields are required!"))
+    }
+
+    // Database Call
+    let user
+    try {
+        user = await userModel.findOne({ email });
+
+        if (!user) {
+            return next(createHttpError(404, "User Not found!"))
+        }
+    } catch {
+        return next(createHttpError(500, "Login - Error while fetching user"))
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return next(createHttpError(400, "Username or password incorrect"))
+    }
+
+    // Create Connection
+    try {
+        const token = sign({ sub: user._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+        })
+
+        res.json({ accessToken: token })
+
+    } catch {
+        return next(createHttpError(500, "Error while signing JWT token"))
+    }
+
 }
 
 export { createUser, loginUser };
